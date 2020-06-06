f101bf2ee914a0bcd4c04d04cd06a4427d9146ff
Fresh-it-up <f.resch@gmx.at>
2020-06-06 14:16:23 +0200

    Begin Implementation of Spells with Kinds as Link

--- resources/js/components/CreateSpell.vue
+++ resources/js/components/CreateSpell.vue
@@ -34,16 +34,26 @@
                             </div>
 
                             <div class="field" >
-                                <label class="label" for="kind_id">Kind ID</label>
-                                <div class="control">
-                                    <input id="kind_id"
-                                           v-model="form.kind_id"
-                                           class="input"
-                                           v-bind:class="{ 'is-danger': form.errors.has('kind_id')}"
-                                           type="text" autofocus>
+                                <label class="label" for="dropdown-menu">Kind</label>
+                                <div class="dropdown is-right box" v-bind:class="{ 'is-active': isDropdownActive }">
+                                    <div class="dropdown-trigger" @click="isDropdownActive=!isDropdownActive" >
+                                        <a aria-haspopup="true" aria-controls="{ 'dropdown-menu'}">
+                                            <span id="selectedOne">{{this.selectedKind == null ? "Select Kind" : this.selectedKind.name }}</span>
+                                            <span class="icon is-small">
+                                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                                            </span>
+                                        </a>
+                                    </div>
+                                    <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                                        <div class="dropdown-content">
+                                            <div v-for="kind in kinds" :key="kind.id">
+                                                      <a href="#" class="dropdown-item " @click="dropOnChange(kind.id, kind.name)">
+                                                           {{kind.name}}
+                                                      </a>
+                                             </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <p class="help is-danger" v-if="form.errors.has('kind_id')"
-                                   v-text="form.errors.get('name')"/>
                             </div>
 
                             <div class="field">
@@ -64,6 +74,7 @@
 </template>
 
 <script>
+
     let form = new Form({
         'id': '',
         'name': '',
@@ -82,30 +93,35 @@
             currentSpell: {
                 required: false,
                 type: Object
-            },
+            }
         },
         data() {
             return {
                 edit: undefined,
                 form: form,
-                url: ''
+                url: '',
+                selectedKind:'',
+                kinds:[],
+                isDropdownActive: false,
             }
         },
         created() {
             this.edit = this.isEditable;
+            this.fetchKinds();
+
+            console.log('Current URl: ' + this.url);
             if (this.edit) {
                 this.url = '/spell/' + this.currentSpell.slug;
-
                 this.form.id = this.currentSpell.id;
                 this.form.name = this.currentSpell.name;
                 this.form.kind_id = this.currentSpell.kind_id;
                 this.form.quote = this.currentSpell.quote;
                 this.form.description = this.currentSpell.description;
-
             } else {
                 this.url = '/spell';
+                this.selectedKind = null;
             }
-            console.log('Current URl: ' + this.url);
+
         },
         methods: {
             submit() {
@@ -118,23 +134,46 @@
                     this.form.post(this.url)
                         .then(response => {
                             this.url = '/spell/' + response.slug;
-                            console.log(response);
-
-                            this.form.id = response.spell_id;
+                            this.form.id = response.id;
                             this.form.name = response.name;
                             this.form.kind_id = response.kind_id;
                             this.form.quote = response.quote;
                             this.form.description = response.description;
-
                             this.edit = true;
-
                             this.form.noReset = ['id', 'name', 'description', 'kind_id', 'quote'];
-
                             window.history.pushState("", "", this.url);
                         });
                     window.location.href = '/spell';
                 }
             },
+            dropOnChange(kindID, kindName){
+                console.log("Incoming: " + kindID);
+                console.log("Form: " + this.form.kind_id);
+                this.form.kind_id = kindID;
+                console.log("Form AFTER: " + this.form.kind_id);
+                document.getElementById("selectedOne").innerText = kindName;
+            },
+            fetchKinds(uri){
+                uri = uri || '/list/kind'
+                console.log("fetching " + uri);
+                fetch(uri)
+                    .then(res => res.json())
+                    .then(res => {
+                        console.log("response: " + res);
+                        this.kinds = res;
+                        if(this.edit){
+                            this.setKindname();
+                        }
+                    })
+
+                    .catch(err => console.log(err));
+            },
+            setKindname(){
+                let knd = this.kinds.filter(kind =>{
+                    return parseInt(kind.id) === parseInt(this.currentSpell.kind_id);
+                });
+                this.selectedKind = knd[0];
+            }
         }
     }
 </script>
--- resources/js/components/ListSpells.vue
+++ resources/js/components/ListSpells.vue
@@ -7,7 +7,7 @@
                 <th>Quote</th>
                 <th>Created</th>
                 <th>Updated</th>
-                <th>Kind-IDs</th>
+                <th>Kindname</th>
                 <th>Edit</th>
                 <th>Delete</th>
             </tr>
@@ -21,7 +21,9 @@
                 <td>{{spell.quote}}</td>
                 <td>{{spell.created_at}}</td>
                 <td>{{spell.updated_at}}</td>
-                <td>{{spell.kind_id}}</td>
+                <td>
+                   <a :href="'/kind/' + getKind(spell.kind_id).slug" :name="getKind(spell.kind_id).name" :description="getKind(spell.kind_id).description" v-text="getKind(spell.kind_id).name"/>
+                </td>
                 <td>
                     <a :href="'/spell/' + spell.slug + '/edit'" class="button is-info is-outlined is-small">Edit</a>
                 </td>
@@ -35,6 +37,8 @@
 </template>
 
 <script>
+
+
     let form = new Form({
         'name': '',
         'quote': '',
@@ -48,14 +52,42 @@
                 required: true
             }
         },
+        data() {
+          return {
+            kinds:[]
+          }
+        },
+        created() {
+          this.fetchKinds();
+        },
         methods: {
             deleteSpell(spell) {
                 spell = spell || this.spell;
                 if(confirm('Are you sure?')){
                     console.log('deleting ' + spell.slug);
                     form.delete(`/spell/${spell.slug}`);
+                    window.location.href = '/spell';
                 }
-            }
+            },
+            fetchKinds(uri){
+            uri = uri || '/list/kind'
+            console.log("fetching " + uri);
+            fetch(uri)
+                .then(res => res.json())
+                .then(res => {
+                    console.log("response: " + res);
+                    this.kinds = res;
+                })
+                .catch(err => console.log(err));
+            },
+            getKind(kindID){
+                if(this.kinds.length <= 0)
+                    return kindID;
+                let knd = this.kinds.filter(kind =>{
+                    return parseInt(kind.id) === parseInt(kindID);
+                });
+                return knd[0];
+            },
         }
     }
 </script>
